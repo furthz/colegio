9186a23710f1fe83508c5266879dc36ec6f29171
Wilber <wilber.carrascal@gmail.com>
2017-08-18 16:54:12 -0500

    Add Forms and Views

--- src/cash/views.py
+++ src/cash/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse_lazy
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import (
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from .models import Remesa
+from .forms import RemesaForm
 
-# Create your views here.
+
+class RemesaListView(ListView):
+    model = Remesa
+    #template_name = 'remesas/remesa_list.html'
+
+
+class RemesaDetailView(DetailView):
+    model = Remesa
+
+
+class RemesaCreationView(CreateView):
+    model = Remesa
+    form_class = RemesaForm
+    success_url = reverse_lazy('remesas:list')
+
+
+class RemesaUpdateView(UpdateView):
+    model = Remesa
+    success_url = reverse_lazy('remesas:list')
+    fields = ['id_remesa', 'id_persona', 'id_movimiento', 'fechacreacion', 'monto', 'comentario']
+
+
+class RemesaDeleteView(DeleteView):
+    model = Remesa
+    success_url = reverse_lazy('remesas:list')
