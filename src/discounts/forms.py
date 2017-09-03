11041d4d2c225f87dbac47edcf74e14f1897e756
Stoker <marco.apolinario.l@gmail.com>
2017-09-02 20:05:25 -0500

    Registrar Pago

---
+++ src/discounts/forms.py
@@ -0,0 +1,26 @@
+from django import forms
+from enrollment.models import Servicio
+from enrollment.models import TipoServicio
+from enrollment.models import Matricula
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from django.forms import ModelForm
+from utils.models import TiposNivel
+from django.utils.translation import ugettext_lazy as _
+
+##############################################################
+#       Solicitar Descuentos
+##############################################################
+
+
+
+
+
+
+
+
+##############################################################
+#       Aprobar Descuentos
+##############################################################
+
+
