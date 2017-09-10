c7473d23c9d173938ac765b81f98fbcf54b047b5
Joel Salazar <ebmymt@gmail.com>
2017-09-10 18:32:44 -0500

    Actualizando segun validaciones

--- src/income/forms.py
+++ src/income/forms.py
@@ -106,72 +106,11 @@ class CuentasCobrarPromotorDetalleForm(forms.Form):
         self.fields['estado'].widget.attrs.update({'class': 'form-control'})
 
 class CuentasCobrarPadresForm(forms.Form):
-    """
-    alumno = forms.ModelChoiceField(
-        label='Alumnos',
-        queryset=None,
-        required=True, )"""
+
     alumno = forms.CharField()
-    anio = forms.CharField()
     mes = forms.CharField()
     estado = forms.CharField()
 
-    def ChoiceAnio(self):
-        MY_CHOICES = (
-            ('Todos', 'Todos'),
-            ('2017', '2017'),
-            ('2016', '2016'),
-        )
-        return MY_CHOICES
-
-    def ChoiceMes(self):
-        MY_CHOICES = (
-            ('Todos', 'Todos'),
-            ('Enero', 'Enero'),
-            ('Febrero', 'Febrero'),
-            ('Marzo', 'Marzo'),
-            ('Abril', 'Abril'),
-            ('Mayo', 'Mayo'),
-            ('Junio', 'Junio'),
-            ('Julio', 'Julio'),
-            ('Agosto', 'Agosto'),
-            ('Setiembre', 'Setiembre'),
-            ('Octubre', 'Octubre'),
-            ('Noviembre', 'Noviembre'),
-            ('Diciembre', 'Diciembre'),
-        )
-        return MY_CHOICES
-
-    def ChoiceEstado(self):
-        MY_CHOICES = (
-            ('Todos', 'Todos'),
-            ('Pagado', 'Pagado'),
-            ('No_pagado', 'No pagado'),
-        )
-        return MY_CHOICES
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['anio'] = forms.ChoiceField(choices=self.ChoiceAnio())
-        self.fields['mes'] = forms.ChoiceField(choices=self.ChoiceMes())
-        self.fields['estado'] = forms.ChoiceField(choices=self.ChoiceEstado())
-        self.fields['alumno'].widget.attrs.update({'class': 'form-control'})
-        self.fields['anio'].widget.attrs.update({'class': 'form-control'})
-        self.fields['mes'].widget.attrs.update({'class': 'form-control'})
-        self.fields['estado'].widget.attrs.update({'class': 'form-control'})
-        """
-        user = get_current_user()
-        logger.debug("Usuario: " + user.name)
 
-        profile = Profile.objects.get(user=user)
-        logger.debug("profile: " + str(profile.id_persona))
-
-        apoderado = Apoderado.objects.get(persona=profile)
-        logger.debug("apoderado: " + str(apoderado.id_apoderado))
-
-        alumnos = apoderado.alumnos.all()
-        """
-        self.helper = FormHelper()
-        self.helper.form_id = "alumno"
-        self.helper.form_method = "post"
-        #self.fields['alumno'].queryset = alumnos
