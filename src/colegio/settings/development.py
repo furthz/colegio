ef9c11bb32cff62eb78ff9b979cd6e35700e73bb
Raul Talledo <raul.talledo@soapros.pe>
2017-09-20 13:36:56 -0500

    ip internal

--- src/colegio/settings/development.py
+++ src/colegio/settings/development.py
@@ -28,7 +28,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 THUMBNAIL_DEBUG = True
 
 # Allow internal IPs for debugging
-INTERNAL_IPS = ['127.0.0.1']
+INTERNAL_IPS = ['127.0.0.1','201.240.145.214']
 
 
 # Log everything to the logs directory at the top
