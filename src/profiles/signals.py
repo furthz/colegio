9e68c0aa7993c3a2eb95265036c9ca5a6415b931
Wilber <wilber.carrasca@gmail.com>
2017-09-24 20:03:32 -0500

    Final Patch

--- src/profiles/signals.py
+++ src/profiles/signals.py
@@ -15,9 +15,10 @@ def create_profile_handler(sender, instance, created, **kwargs):
         return
     # Create the profile object, only if it is newly created
     if settings.IS_TESTING == False:
+        """
         profile = models.Profile(user=instance)
         profile.save()
-
+"""
     #persona = Persona(user=instance)
     #persona.save()
     logger.info('New user profile for {} created'.format(instance))
