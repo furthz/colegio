70b610c7e266c79722c0444c89296751ed6cae1f
Raul Talledo <raul.talledo@soapros.pe>
2017-09-26 21:10:11 -0500

    se agregaron los prefijos para el admin

--- src/colegio/settings/production.py
+++ src/colegio/settings/production.py
@@ -8,7 +8,7 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 
 # Must mention ALLOWED_HOSTS in production!
-ALLOWED_HOSTS = ["13.72.73.14"]
+ALLOWED_HOSTS = ["mundopixel.pe"]
 
 # Cache the templates in memory for speed-up
 #loaders = [
