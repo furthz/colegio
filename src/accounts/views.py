40149c4cb7fdccda040517359b33bc4a7f0ad607
Raul Talledo <raul.talledo@soapros.pe>
2017-10-02 18:45:47 -0500

    correccion accounts/views.py

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -256,7 +256,7 @@ class RegistroUsuario(CreateView):
 
             usuario.save()
 
-            grupos = form.data['groups']
+            grupos = form.data_form['groups']
 
             #from django.contrib.auth.models import Group
 
