85a6c1e47c6b2020fbb586d038e79be88ae6e026
Wilber <wilber.carrascal@gmail.com>
2017-10-12 12:18:47 -0500

    AE_New_Folders

---
+++ src/AE_chat/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AeChatConfig(AppConfig):
+    name = 'AE_chat'
---
+++ src/AE_social/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AeSocialConfig(AppConfig):
+    name = 'AE_social'
