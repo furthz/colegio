eaaa31a9950ef53198c5f3f8a87addb313710f88
Stoker <marco.apolinario.l@gmail.com>
2017-10-15 19:45:05 -0500

    Relacion Curso Docente

--- src/AE_academico/forms.py
+++ src/AE_academico/forms.py
@@ -1,7 +1,7 @@
 """
 from django import forms
 from AE_academico.models import Aula
-
+from AE_academico.models import CursoDocente
 
 class AulaForm(forms.ModelForm):
     class Meta:
@@ -22,4 +22,23 @@ class AulaForm(forms.ModelForm):
             'nombre': forms.TextInput(attrs={'class': 'form-control'}),
         }
 
+
+class CursoDocenteForm(forms.ModelForm):
+    class Meta:
+        model = CursoDocente
+
+        fields = [
+            'docente',
+            'curso',
+        ]
+
+        labels = {
+            'docente': 'Docente',
+            'curso': 'Curso',
+        }
+
+        widgets = {
+            'docente': forms.Select(attrs={'class': 'form-control'}),
+            'curso': forms.Select(attrs={'class': 'form-control'}),
+        }
 """
--- src/AE_academico/views.py
+++ src/AE_academico/views.py
@@ -4,9 +4,13 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
 
 from AE_academico.forms import AulaForm
+from AE_academico.forms import CursoDocenteForm
 from AE_academico.models import Aula
+from AE_academico.models import CursoDocente
+from AE_academico.models import Curso
+from register.models import Docente, Personal, PersonalColegio
 from django.conf import settings
-from utils.middleware import validar_roles, get_current_request
+from utils.middleware import validar_roles, get_current_request, get_current_colegio, get_current_user
 from django.http import HttpResponseRedirect
 from utils.views import MyLoginRequiredMixin
 
@@ -83,6 +87,35 @@ class AulaDeleteView(DeleteView):
         roles = ['promotor', 'director', 'administrativo']
         if validar_roles(roles=roles):
             return super(AulaDeleteView, self).get(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect(settings.REDIRECT_PERMISOS)
+
+
+#################################################
+#####            CRUD DE CURSO DOCENTE      #####
+#################################################
+
+class CursoDocenteCreateView(CreateView):
+    model = CursoDocente
+    form_class = CursoDocenteForm
+    success_url = reverse_lazy('academic:aula_list')
+    template_name = 'cursodocente_form.html'
+
+    def get(self, request, *args, **kwargs):
+        roles = ['promotor', 'director', 'administrativo', 'tesorero']
+        if validar_roles(roles=roles):
+            personalcolegio = PersonalColegio.objects.filter(colegio_id=get_current_colegio(), activo=True)
+            personal = []
+            for personalcol in personalcolegio:
+                personal.append(personalcol.personal)
+            cursos = Curso.objects.filter(aula__tipo_servicio__colegio_id=get_current_colegio(), activo= True)
+            docentes = Docente.objects.filter(empleado=personal)
+            return render(request, template_name=self.template_name, context={
+                'form': self.form_class,
+                'docentes': docentes,
+                'cursos': cursos,
+            })
+
         else:
             return HttpResponseRedirect(settings.REDIRECT_PERMISOS)
 
