ab3d70c0116fc183609302474665cd8d4c7c206e
Joel Salazar <ebmymt@gmail.com>
2017-12-03 13:29:38 -0500

    Corrigiendo migrations APIs

--- src/APIs/migrations/0001_initial.py
+++ src/APIs/migrations/0001_initial.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-                     ('profile','0001_initial'),
+                     ('profiles','0001_initial'),
     ]
 
     operations = [
